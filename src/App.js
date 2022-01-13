4e2158899b4452ab2ab5df7faf4964190c2cbe32
AlejaeMC <alejandroanayadom@gmail.com>
2022-01-13 07:01:04 -0700

    Hook useState

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {EventosES6, EventosES7, MasSobreEventos} from './components/Eventos';
 import ComunicacionComponentes from './components/ComunicacionComponentes';
 import CicloVida from './components/CicloVida';
 import AjaxApis from './components/AjaxApis';
+import ContadorHooks from './components/ContadorHooks';
 import './App.css';
 
 function App() {
@@ -60,6 +61,8 @@ function App() {
         <CicloVida/>
         <hr />
         <AjaxApis/>
+        <hr />
+        <ContadorHooks/>
       </section>
       </header>
     </div>
