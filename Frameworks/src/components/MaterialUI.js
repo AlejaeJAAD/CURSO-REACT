e71958dfebd308429eaec7b27ece8fb82e9ee9a5
AlejaeMC <alejandroanayadom@gmail.com>
2022-01-19 05:23:28 -0700

    React Bootstrap - Material UI

---
+++ Frameworks/src/components/MaterialUI.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MaterialCard from "./MaterialCard";
+import MaterialDrawer from "./MaterialDrawer";
+
+export default function MaterialUI() {
+  return (
+    <>
+      <hr />
+      <h2>Material UI</h2>
+      <MaterialCard />
+      <MaterialDrawer />
+    </>
+  );
+}
